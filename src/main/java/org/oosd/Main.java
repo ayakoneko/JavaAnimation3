499d010ebb718a6303b0e56ba478aea3c6a19887
LarryAtGU <l.wen@griffith.edu.au>
2025-07-06 16:52:28 +1000

    finish lab3 features

--- src/main/java/org/oosd/Main.java
+++ src/main/java/org/oosd/Main.java
@@ -4,7 +4,8 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -20,6 +21,9 @@ public class Main extends Application {
 
     private double dx = 3;       // X velocity
     private double dy = 3;       // Y velocity
+    private boolean hasShadow = false;
+    private String colorString = "RED";
+    private int size = 10;
 
     private final double fieldWidth = 400;
     private final double fieldHeight = 300;
@@ -39,12 +43,77 @@ public class Main extends Application {
     private void showMainScreen() {
         VBox mainScreen = new VBox(10);
         mainScreen.setPadding(new Insets(20));
-        Button startButton = new Button("Start Game");
 
+        Label label = new Label("Main Screen");
+
+        Button startButton = new Button("Start Game");
         startButton.setOnAction(e -> showGameScreen());
 
-        mainScreen.getChildren().add(startButton);
+        Button configButton = new Button("Configuration");
+        configButton.setOnAction(e -> showConfigScreen());
+
+        Button exitButton = new Button("Exit");
+        exitButton.setOnAction(e -> System.exit(0));
+
+
+        mainScreen.getChildren().addAll(label, startButton, configButton, exitButton);
         root.getChildren().setAll(mainScreen);
+    }
+
+    private Color getColor() {
+        return switch (colorString) {
+            case "RED" -> Color.RED;
+            case "GREEN" -> Color.GREEN;
+            case "BLUE" -> Color.BLUE;
+            default -> Color.RED;
+        };
+    }
+
+    private void showConfigScreen() {
+        VBox configScreen = new VBox(10);
+        configScreen.setPadding(new Insets(20));
+        Label label = new Label("Configuration");
+        CheckBox cbShadow = new CheckBox("Enable Shadow");
+        cbShadow.setSelected(hasShadow);
+        cbShadow.setOnAction(e -> hasShadow = cbShadow.isSelected());
+
+        Label colorLabel = new Label("Color");
+
+        RadioButton rbRed = new RadioButton("Red");
+        RadioButton rbGreen = new RadioButton("Green");
+        RadioButton rbBlue = new RadioButton("Blue");
+        ToggleGroup colorGroup = new ToggleGroup();
+        rbBlue.setToggleGroup(colorGroup);
+        rbRed.setToggleGroup(colorGroup);
+        rbGreen.setToggleGroup(colorGroup);
+        rbBlue.setOnAction(e -> colorString = "BLUE");
+        rbRed.setOnAction(e -> colorString = "RED");
+        rbGreen.setOnAction(e -> colorString = "GREEN");
+        switch (colorString) {
+            case "RED" -> rbRed.setSelected(true);
+            case "GREEN" -> rbGreen.setSelected(true);
+            case "BLUE" -> rbBlue.setSelected(true);
+            default -> rbRed.setSelected(true);
+        }
+
+        Label sizeLabel = new Label("Size: " + size);
+        Slider sizeSlider = new Slider(5, 20, size);
+        sizeSlider.setShowTickLabels(true);
+        sizeSlider.setShowTickMarks(true);
+        sizeSlider.setMajorTickUnit(5);
+        sizeSlider.setMinorTickCount(1);
+        sizeSlider.valueProperty().addListener(
+                (obs, oldVal, newVal) -> {
+                    size = newVal.intValue();
+                    sizeLabel.setText("Size: " + size);
+                }
+        );
+
+        Button back = new Button("Back");
+        back.setOnAction(e -> showMainScreen());
+
+        configScreen.getChildren().addAll(label, cbShadow, colorLabel, rbRed, rbGreen, rbBlue, sizeLabel, sizeSlider, back);
+        root.getChildren().setAll(configScreen);
     }
 
     private void showGameScreen() {
@@ -56,9 +125,15 @@ public class Main extends Application {
         field.setStroke(Color.BLACK);
 
         // Create red ball
-        Circle ball = new Circle(10, Color.RED);
+        Circle ball = new Circle(size, getColor());
         ball.setCenterX(fieldWidth / 2);
         ball.setCenterY(fieldHeight / 2);
+        if (hasShadow) {
+            DropShadow shadow = new DropShadow();
+            shadow.setOffsetX(5);
+            shadow.setOffsetY(5);
+            ball.setEffect(shadow);
+        }
 
         Button backButton = new Button("Back");
         backButton.setLayoutX(10);
@@ -73,26 +148,25 @@ public class Main extends Application {
         // Key control
         scene.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.UP) {
-                dx = 0;
-                dy = -3;
+                dy = dy > 0 ? dy + 1 : dy - 1;
             } else if (e.getCode() == KeyCode.DOWN) {
-                dx = 0;
-                dy = 3;
+                dy = dy < 0 ? dy + 1 : dy - 1;
             } else if (e.getCode() == KeyCode.LEFT) {
-                dx = -3;
-                dy = 0;
+                dx = dx > 0 ? dx - 1 : dx + 1;
             } else if (e.getCode() == KeyCode.RIGHT) {
-                dx = 3;
-                dy = 0;
+                dx = dx < 0 ? dx - 1 : dx + 1;
             }
         });
 
         timer = new AnimationTimer() {
+            int count;
+
             @Override
             public void handle(long now) {
                 double nextX = ball.getCenterX() + dx;
                 double nextY = ball.getCenterY() + dy;
-
+                count++;
+                if (count % 10 == 0) System.out.println("Count: " + count);
                 // Bounce off edges
                 if (nextX - ball.getRadius() < 0 || nextX + ball.getRadius() > fieldWidth) {
                     dx = -dx;
