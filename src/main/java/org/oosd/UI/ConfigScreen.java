39ec3ac2d44ac7326434132f3e900e463492af2b
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-09-02 20:32:40 +1000

    ConfigScreen refactoring with ScreenWithGame interface

--- src/main/java/org/oosd/UI/ConfigScreen.java
+++ src/main/java/org/oosd/UI/ConfigScreen.java
@@ -1,58 +1,112 @@
 package org.oosd.UI;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import org.oosd.model.Game;
 
-public class ConfigScreen {
+public class ConfigScreen implements ScreenWithGame {
+    private final Frame parent;
+    private Game game;
+    private final BorderPane configScreen;
+    private Screen mainScreen;
 
-    private void showConfigScreen() {
-        VBox configScreen = new VBox(10);
-        configScreen.setPadding(new Insets(20));
+    public ConfigScreen(Frame frame) {
+        this.parent = frame;
+        this.configScreen = new BorderPane();
+    }
+
+    private StackPane buildTop() {
         Label label = new Label("Configuration");
+
+        StackPane top = new StackPane(label);
+        top.setPadding(new Insets(10, 0, 0, 0));
+        top.setAlignment(Pos.CENTER);
+        return top;
+    }
+
+    private StackPane buildBottom() {
+        Button back = new Button("Back");
+        back.setOnAction(e -> parent.showScreen(mainScreen));
+
+        StackPane bottom = new StackPane(back);
+        bottom.setAlignment(Pos.CENTER);
+        bottom.setPadding(new Insets(0, 0, 20, 0));
+        return bottom;
+    }
+
+    private VBox buildCentre() {
+        VBox centre = new VBox(14);
+        centre.setPadding(new Insets(20));
 
         CheckBox cbShadow = new CheckBox("Enable Shadow");
         cbShadow.setSelected(game.isHasShadow());
         cbShadow.setOnAction(e -> game.setHasShadow(cbShadow.isSelected()));
 
-        Label colorLabel = new Label("Color");
-
+        HBox colourPane = new HBox(20);
+        Label colourLabel = new Label("Color:");
         RadioButton rbRed = new RadioButton("Red");
         RadioButton rbGreen = new RadioButton("Green");
         RadioButton rbBlue = new RadioButton("Blue");
-        ToggleGroup colorGroup = new ToggleGroup();
-        rbBlue.setToggleGroup(colorGroup);
-        rbRed.setToggleGroup(colorGroup);
-        rbGreen.setToggleGroup(colorGroup);
-        rbBlue.setOnAction(e -> game.setColorString("BLUE"));
+        ToggleGroup group = new ToggleGroup();
+        rbRed.setToggleGroup(group);
+        rbGreen.setToggleGroup(group);
+        rbBlue.setToggleGroup(group);
         rbRed.setOnAction(e -> game.setColorString("RED"));
         rbGreen.setOnAction(e -> game.setColorString("GREEN"));
+        rbBlue.setOnAction(e -> game.setColorString("BLUE"));
         switch (game.getColorString()) {
             case "RED" -> rbRed.setSelected(true);
             case "GREEN" -> rbGreen.setSelected(true);
             case "BLUE" -> rbBlue.setSelected(true);
             default -> rbRed.setSelected(true);
         }
+        colourPane.getChildren().addAll(colourLabel, rbRed, rbGreen, rbBlue);
 
+        HBox sizePane = new HBox(10);
         Label sizeLabel = new Label("Size: " + game.getSize());
         Slider sizeSlider = new Slider(5, 20, game.getSize());
         sizeSlider.setShowTickLabels(true);
         sizeSlider.setShowTickMarks(true);
         sizeSlider.setMajorTickUnit(5);
         sizeSlider.setMinorTickCount(1);
-        sizeSlider.valueProperty().addListener(
-                (obs, oldVal, newVal) -> {
-                    int newSize = newVal.intValue();
-                    game.setSize(newSize);
-                    sizeLabel.setText("Size: " + newSize);
-                }
-        );
+        sizeSlider.valueProperty().addListener((obs, ov, nv) -> {
+            int s = nv.intValue();
+            game.setSize(s);
+            sizeLabel.setText("Size: " + s);
+        });
+        HBox.setHgrow(sizeSlider, Priority.ALWAYS);
+        sizePane.getChildren().addAll(sizeLabel, sizeSlider);
 
-        Button back = new Button("Back");
-        back.setOnAction(e -> showMainScreen());
+        centre.getChildren().addAll(cbShadow, colourPane, sizePane);
+        return centre;
+    }
 
-        configScreen.getChildren().addAll(label, cbShadow, colorLabel, rbRed, rbGreen, rbBlue, sizeLabel, sizeSlider, back);
-        root.getChildren().setAll(configScreen);
+    private void buildScreen() {
+        configScreen.setTop(buildTop());
+        configScreen.setCenter(buildCentre());
+        configScreen.setBottom(buildBottom());
+    }
+
+
+    @Override
+    public Node getScreen() {
+        return configScreen;
+    }
+
+    @Override
+    public void setRoute(String path, Screen screen) {
+        if ("back".equals(path)) {
+            this.mainScreen = screen;
+            buildScreen();
+        }
+    }
+
+    @Override
+    public void setGame(Game game) {
+        this.game = game;
     }
 
 }
