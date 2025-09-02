06ebc7deecfa98c1c589e52bdaf8662f6806ab87
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-09-02 20:23:55 +1000

    MainScreen refactoring with Screen interface

--- src/main/java/org/oosd/UI/MainScreen.java
+++ src/main/java/org/oosd/UI/MainScreen.java
@@ -1,30 +1,51 @@
 package org.oosd.UI;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class MainScreen {
+public class MainScreen implements Screen {
+    private VBox mainScreen;
+    private Screen configScreen, gameScreen;
+    private final Frame parent;
 
-    private void showMainScreen() {
-        VBox mainScreen = new VBox(10);
+    public MainScreen(Frame frame) {
+        this.parent = frame;
+
+        mainScreen = new VBox(10);
+        mainScreen.setAlignment(Pos.CENTER);
         mainScreen.setPadding(new Insets(20));
+        mainScreen.setSpacing(20);
 
         Label label = new Label("Main Screen");
 
         Button startButton = new Button("Start Game");
-        startButton.setOnAction(e -> showGameScreen());
+        startButton.setOnAction(e -> parent.showScreen(gameScreen));
 
         Button configButton = new Button("Configuration");
-        configButton.setOnAction(e -> showConfigScreen());
+        configButton.setOnAction(e -> parent.showScreen(configScreen));
 
         Button exitButton = new Button("Exit");
-        exitButton.setOnAction(e -> System.exit(0));
-
+        exitButton.setOnAction(e -> parent.showExitConfirmation());
 
         mainScreen.getChildren().addAll(label, startButton, configButton, exitButton);
-        root.getChildren().setAll(mainScreen);
+    }
+
+    @Override
+    public Node getScreen() {
+        return mainScreen;
+    }
+
+    @Override
+    public void setRoute(String path, Screen screen) {
+        switch (path) {
+            case "config" -> this.configScreen = screen;
+            case "game" -> this.gameScreen = screen;
+            default -> { /* no-op */ }
+        }
     }
 
 }
