ebc643cbe2f4a5f3f5e8ce3386a01cf222f37a9c
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-09-10 11:19:18 +1000

    Setup Env for screen margin setup

---
+++ src/main/java/org/oosd/Env.java
@@ -0,0 +1,7 @@
+package org.oosd;
+
+public class Env {
+    public static final int HORIZONTAL_MARGIN = 10;
+    public static final int TOP_MARGIN = 10;
+    public static final int BOTTOM_MARGIN = 80;
+}
